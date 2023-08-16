87961cb4545c297d70859869025be927004e7c02
Victor Macharete <92468230+Guinhovhrm@users.noreply.github.com>
2023-08-16 12:30:08 +0000

    Exercicio10

---
+++ Exerc10.php
@@ -0,0 +1,9 @@
+<?php 
+    for($i=1;$i<=100000;$i++){ 
+        echo ("$i <br>");
+    }
+    echo("Acabou!");
+    echo("Tempo para carregar os valores: 3 segundos");
+    echo("Tempo sem nada: Instantâneo");
+    echo("Concluí que é por conta do computador ter que processar o for e todos os números");
+?>
